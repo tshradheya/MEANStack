391683a2bde85867ae1f597b6637506b97690b62
Shradheya Thakre <tshradheya@gmail.com>
2017-12-15 12:41:38 +0300

    clean up

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var User  = require('../models/user')
+
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-      if(err) {
-        return res.send('Error!')
-      }
-      res.render('node', {email: doc.email});
-    })
-});
+  res.render('index');
+})
 
-router.post('/', function (req, res, next) {
+/*router.post('/', function (req, res, next) {
   var email = req.body.email;
   var user = new User({
       firstName: 'Shraddy',
@@ -22,5 +17,18 @@ router.post('/', function (req, res, next) {
   user.save();
   res.redirect('/');
 })
+*/
+
+//var User  = require('../models/user')
+
+/*router.get('/', function (req, res, next) {
+    User.findOne({}, function(err, doc) {
+      if(err) {
+        return res.send('Error!')
+      }
+      res.render('node', {email: doc.email});
+    })
+});
+*/
 
 module.exports = router;
