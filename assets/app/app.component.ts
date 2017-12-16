72870b849caa36cf5f5819e6b58e394a2eefcb47
Shradheya Thakre <tshradheya@gmail.com>
2017-12-16 13:30:05 +0300

    added child components

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+import{ Message } from "./messages/message.model";
+
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [`
+        .author {
+            display: inline-block;
+            font-style: itaic;
+            font-size: 12px;
+            width: 80%;
+        }
+        .config {
+            display: inline-block;
+            font-style: right;
+            font-size: 12px;
+            width: 19%;
+        }
+        `
+    ]
 })
 export class AppComponent {
-    
+    message: Message = new Message('Some message', 'Shrad');
 }
---
+++ assets/app/messages/message.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+import { Message } from "./message.model";
+
+@Component({
+    selector: 'app-message',
+    templateUrl: './message.component.html'
+    styles: [`
+        .author {
+            display: inline-block   ;
+            font-style: itaic;
+            font-size: 12px;
+            width: 80%;
+        }
+        .config {
+            display: inline-block;
+            font-style: right;
+            font-size: 12px;
+            width: 19%;
+        }
+        `
+})
+export class MessageComponent {
+    @Input() message: Message;
+    @Output() editClicked = new EventEmitter<string>();
+
+    onEdit() {
+        this.editClicked.emit('Warning! You will edit the chat message')
+        //alert('Warning! You will edit the chat message');
+    }
+}
